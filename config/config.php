85c4c1e5b02ff3d3dbee89f1e01ef44c3df80b54
CagsMJ <150440362+03112002mjc@users.noreply.github.com>
2024-06-09 20:58:23 +0800

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,14 +1,17 @@
 <?php
+
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "notebook";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn =  mysqli_connect($servername, $username, $password, $dbname);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!$conn) {
+    echo "Connection Failed!";
 }
+return $conn;
 ?>
