42d2a0a5df4a84855495af031bfdf3d6486ac9d8
CagsMJ <150440362+03112002mjc@users.noreply.github.com>
2024-06-09 20:47:46 +0800

    Delete function.php

--- function.php
+++
@@ -1,8 +0,0 @@
-<?php
-function checkRole($role) {
-    if (!isset($_SESSION['user_role']) || $_SESSION['user_role'] !== $role) {
-        header("Location: index.php");
-        exit;
-    }
-}
-?>
